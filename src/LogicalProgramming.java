186dd251ce7267219bc844b00e6a49b9ad78da72
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-28 17:25:00 +0530

    Added : code to check for a Perfect Number

--- src/LogicalProgramming.java
+++ src/LogicalProgramming.java
@@ -5,6 +5,8 @@ public class LogicalProgramming {
     public static void main(String[] args) {
         LogicalProgramming logicalProgramming=new LogicalProgramming();
         logicalProgramming.fibonacciSeries();
+        logicalProgramming.perfectNumber();
+
     }
      void fibonacciSeries() {
         int number = 0;
@@ -14,6 +16,23 @@ public class LogicalProgramming {
             number = number + i;
             System.out.print(number + " ");
         }
+    }
+    void perfectNumber() {
+        System.out.println("Enter the number -");
+        int numb = input.nextInt();
+        int a = 1;
+        int sum = 0;
+        for (int i = 1; i < numb; i++) {
+            if (numb % a == 0) {
+                System.out.print(a + " ");
+                sum = sum + a;
+                a = a + 1;
+            } else a = a + 1;
+        }
+        System.out.println(" ");
+        if (sum == numb) {
+            System.out.println(numb + " is a perfect number ");
+        } else System.out.print(numb + " is not a perfect number");
     }
 
 }
